a9e18ca7f0ded5c71404a9e80e3cbe788c905853
Abeer958 <84776419+AbeerWebDev@users.noreply.github.com>
2023-10-20 20:02:13 +0400

    commit

--- myapp/src/app/page.jsx
+++ myapp/src/app/page.jsx
@@ -1,5 +1,23 @@
+import styles from './page.module.css'
+import Image from 'next/image';
+import Hero from '/public/Hero.png'
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} className={styles.img} alt="" />
+      </div>
+    </div>
+  );
 }
--- myapp/src/components/footer/Footer.jsx
+++ myapp/src/components/footer/Footer.jsx
@@ -1,12 +1,42 @@
 "use client"
 import React from 'react'
-import styles from './page.module.css'
+import styles from './footer.module.css'
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 BiteSize. All rights reserved.</div>
-      <div></div>
+      <div className={styles.socials}>
+        <Image
+          className={styles.icon}
+          src="/1.png"
+          width={15}
+          height={15}
+          alt="Bite Size Facebook Account"
+        />
+        <Image
+          className={styles.icon}
+          src="/2.png"
+          width={15}
+          height={15}
+          alt="Bite Size Instagram Account"
+        />
+        <Image
+          className={styles.icon}
+          src="/3.png"
+          width={15}
+          height={15}
+          alt="Bite Size Twitter Account"
+        />
+        <Image
+          className={styles.icon}
+          src="/4.png"
+          width={15}
+          height={15}
+          alt="Bite Size Youtube Account"
+        />
+      </div>
     </div>
   );
 }

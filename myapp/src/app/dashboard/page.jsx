b2439285226f37961fcbc78c51d2065e7b488b1d
Abeer958 <84776419+AbeerWebDev@users.noreply.github.com>
2023-10-31 18:04:08 +0400

    commit

--- myapp/src/app/dashboard/page.jsx
+++ myapp/src/app/dashboard/page.jsx
@@ -2,15 +2,8 @@
 import React from 'react'
 import styles from "./page.module.css";
 import { useSession } from 'next-auth/react';
-// import useSWR from "swr";
 
 const Dashboard = () => {
-  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  // const { data, error, isLoading } = useSWR(
-  //   "https://jsonplaceholder.typicode.com/posts",
-  //   fetcher
-  // );
-
   const session = useSession()
   console.log(session)
   return <div className={styles.container}></div>;
